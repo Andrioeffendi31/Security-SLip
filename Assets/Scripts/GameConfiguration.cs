5a7feffe45c94ca0d87e4894c95f0cdc72802f65
Lemuel Lancaster <32617101+Itsakaseru@users.noreply.github.com>
2021-03-22 17:03:16 +0700

    Add ability to adjust the game time speed via configurator, implements main menu button

--- Assets/Scripts/GameConfiguration.cs
+++ Assets/Scripts/GameConfiguration.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 public static class GameConfiguration
 {
     public static bool DebugMode { get; set; }
+    public static int secondPerRealSecond { get; set; } = 70;
 
     public static void ResetToDefault()
     {
         DebugMode = false;
+        secondPerRealSecond = 70;
     }
 }
