743ec286536fb8a9b1b3646f14152053ef353000
Lemuel Lancaster <32617101+Itsakaseru@users.noreply.github.com>
2021-04-18 13:50:30 +0700

    Upgrade unity probuilder package

--- Assets/Scripts/Gameplay/Sakii_DEBUG.cs
+++ Assets/Scripts/Gameplay/Sakii_DEBUG.cs
@@ -5,16 +5,8 @@ using UnityEngine;
 
 public class Sakii_DEBUG : MonoBehaviour
 {
-    private readonly InfoRandomizer infoRandomizer = new InfoRandomizer();
-
     private void Start()
     {
-        Character test = infoRandomizer.GetRandomizeCharacter();
-
-        Debug.Log(test.firstName);
-        Debug.Log(test.middleName);
-        Debug.Log(test.lastName);
-
-       
+        
     }
 }
