b343bf26898f09e8134ef430f1379b471967f63c
Lemuel Lancaster <32617101+Itsakaseru@users.noreply.github.com>
2021-04-19 23:48:34 +0700

    Revised game manager and refactor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,92 +6,50 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private ClockSystem clockSystem;
-    private ApprovalSystem ApprovalSystem = new ApprovalSystem();
+    [SerializeField]
+    private GameplayController gameplayController;
 
-    public CharacterGenerator characterGenerator;
-    public CharacterLogic characterLogic;
-    public Character character;
-    public bool isPlaying;
-    public bool allowToChoose;
+    [SerializeField]
+    private AudioManager audioManager;
 
-    public int score = 0;
-    public Image scoreProgressBar;
-
-    public GameObject entryCard;
-    public Text maleCard_fullName;
-    public Text maleCard_expiredDate;
-    public Text femaleCard_fullName;
-    public Text femaleCard_expiredDate;
-
-    private void Awake()
-    {
-        // DontDestroyOnLoad(this);
-    }
+    private bool isPlaying;
 
     private void Start()
     {
-        Cursor.visible = false;
+        Init();
+    }
+
+    private void Init()
+    {
+        DontDestroyOnLoad(this);
+
+        isPlaying = false;
         Cursor.lockState = CursorLockMode.Confined;
-        clockSystem.SetStartDateTime(2021, 3, 23, 7, 0, 0);
-        Debug.Log("Current DateTime : " + clockSystem.GetCurrentDateTime());
-        allowToChoose = false;
-        scoreProgressBar.fillAmount = 0;
     }
 
-    private void Update()
+    public void StartGame(GameplayController gameplayController)
     {
-        if (clockSystem.GetCurrentDateTime().Hour >= 8)
-            // Game Over
-            SceneManager.LoadScene("MainMenu");
+        this.gameplayController = gameplayController;
+        isPlaying = true;
     }
 
-    public void ApplyInfo()
+    public void EndGame()
     {
-        entryCard.SetActive(true);
-        // Male = 0, Female = 1
-        if (character.gender == 0)
-        {
-            maleCard_fullName.text = character.GetFullName();
-            maleCard_expiredDate.text = character.info.expired.ToString("dd/MM/yyyy");
-        } else
-        if (character.gender == 1)
-        {
-            femaleCard_fullName.text = character.GetFullName();
-            femaleCard_expiredDate.text = character.info.expired.ToString("dd/MM/yyyy");
-        }
+        isPlaying = false;
     }
 
-    public void CheckInfo(bool userDecision)
+    public void GoToMainMenu()
     {
-        bool status = ApprovalSystem.isExpired(clockSystem.GetCurrentDateTime(), character.GetInfoExpiredDateTime());
+        SceneManager.LoadScene("MainMenu");
+    }
 
-        switch (userDecision)
-        {
-            case true:
-                characterLogic.AllowedToEntry(true);
-                entryCard.SetActive(false);
-                if (status)
-                {
-                    Debug.Log("WRONG DECISION");
-                    return;
-                }
-                break;
+    public void ShowCursor()
+    {
+        Cursor.visible = true;
+    }
 
-            case false:
-                characterLogic.AllowedToEntry(false);
-                entryCard.SetActive(false);
-                if (!status)
-                {
-                    Debug.Log("WRONG DECISION");
-                    return;
-                }
-                break;
-        }
-
-        // Add score
-        Debug.Log("CORRECT DECISION");
-        score++;
-        scoreProgressBar.fillAmount = (float) score / 10;
+    public void HideCursor()
+    {
+        Cursor.visible = false;
     }
 }
