e453e905a87c8fc59286aa150f5a5f4e1f5a7f5b
afrisanjaya <47937342+afrisanjaya@users.noreply.github.com>
2021-05-11 18:14:57 +0700

    Function SetVolume
    SetVolume Master,SFX,BGM

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
@@ -14,6 +16,26 @@ public class AudioManager : MonoBehaviour
 
     public AudioSource audioSourceBGM,
                        audioSourceSFX;
+
+    public AudioMixer masterMixer;
+
+    public void setMasterVolume(float sliderValue)
+    {
+        masterMixer.SetFloat("Mastervol", Mathf.Log(sliderValue) * 20);
+        PlayerPrefs.SetFloat("Mastervol", sliderValue);
+    }
+
+    public void setBGMVolume(float sliderValue)
+    {
+        masterMixer.SetFloat("BGMvol", Mathf.Log(sliderValue) * 20);
+        PlayerPrefs.SetFloat("BGMvol", sliderValue);
+    }
+
+    public void setSFXVolume(float sliderValue)
+    {
+        masterMixer.SetFloat("SFXvol", Mathf.Log(sliderValue) * 20);
+        PlayerPrefs.SetFloat("SFXvol", sliderValue);
+    }
 
     private void Start()
     {
