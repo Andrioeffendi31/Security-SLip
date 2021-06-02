8ec4cba50b5754f54888f160ff54abd45ef4a07e
Lemuel Lancaster <32617101+Itsakaseru@users.noreply.github.com>
2021-06-03 00:44:58 +0700

    Revised search system for database

--- Assets/Scripts/Gameplay/NameComputer.cs
+++ Assets/Scripts/Gameplay/NameComputer.cs
@@ -1,8 +1,11 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NameComputer : MonoBehaviour
 {
+    private readonly int TOTAL_DUMMY_DATA = 100;
+
     [SerializeField]
     private TextAsset databaseMale;
 
@@ -13,32 +16,45 @@ public class NameComputer : MonoBehaviour
     
     private SearchBar searchBar;
 
+    private List<Data> database_data = new List<Data>();
+
     public GameObject data_prefab;
 
     public GameObject data_content;
 
     public GameObject pending;
 
+    public int data_realIndex;
+
     public string data_realName;
 
     public string data_realID;
 
-    int counter_name = 100;
-
-    void Start()
+    private void Start()
     {
+        GenerateDummyData();
+    }
+
+    private void GenerateDummyData()
+    {
+        InfoRandomizer.DB_Male dbMale = JsonUtility.FromJson<InfoRandomizer.DB_Male>(databaseMale.text);
+        InfoRandomizer.DB_Female dbFemale = JsonUtility.FromJson<InfoRandomizer.DB_Female>(databaseFemale.text);
+
         pending.gameObject.SetActive(false);
         searchAnimator = pending.GetComponent<Animator>();
 
-        foreach(Transform child in data_content.transform){
+        foreach(Transform child in data_content.transform)
+        {
             GameObject.Destroy(child.gameObject);
         }
 
-        for(int i = 0; i < counter_name; i++){
-            InfoRandomizer.DB_Male dbMale = JsonUtility.FromJson<InfoRandomizer.DB_Male>(databaseMale.text);
-            InfoRandomizer.DB_Female dbFemale = JsonUtility.FromJson<InfoRandomizer.DB_Female>(databaseFemale.text);
+        for(int i = 0; i < TOTAL_DUMMY_DATA; i++)
+        {
             Data infoCharater = new Data(i, dbMale, dbFemale);
+            database_data.Add(infoCharater);
+            
             GameObject data = Instantiate(data_prefab);
+
             data.GetComponent<DatabaseContent>().SetID(infoCharater.cardID);
             data.GetComponent<DatabaseContent>().SetNama(infoCharater.firstName + " " + infoCharater.middleName + " " + infoCharater.lastName);
             data.transform.SetParent(data_content.transform);
@@ -46,39 +62,59 @@ public class NameComputer : MonoBehaviour
         }
     }
 
+    public void InjectData(Character character)
+    {
+        this.data_realID = character.GetCardID();
+        this.data_realName = character.GetFullName();
+
+        database_data.Add(new Data(0, character.GetCardID(), character.firstName, character.middleName, character.lastName));
+    }
+
     public void ResetRealData()
     {
         data_realName = "";
+        data_realID = "";
+        data_realIndex = 0;
+
+        database_data.Clear();
+        GenerateDummyData();
     }
 
-    public void SearchResult(string searchForName)
+    public void SearchResult(string searchString)
     {
         pending.gameObject.SetActive(true);
 
         // Set animation level according to search time
         searchAnimator.SetFloat("speedMultiplier", ((float) 10 / GameConfiguration.databaseSearchTime));
         
-        StartCoroutine(Pending(searchForName));
+        StartCoroutine(Pending(searchString));
 
         GameObject data = Instantiate(data_prefab);
 
-        foreach(Transform child in data_content.transform){
+        foreach(Transform child in data_content.transform)
+        {
             GameObject.Destroy(child.gameObject);
         }
     }
 
-    public void SearchDone(string searchForName)
+    public void SearchDone(string searchString)
     {
-        GameObject data = Instantiate(data_prefab);
+        foreach(Transform child in data_content.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
 
-        if(searchForName == data_realName){
-            foreach(Transform child in data_content.transform){
-                GameObject.Destroy(child.gameObject);
+        foreach(Data data in database_data)
+        {
+            if (data.GetFullName().Contains(searchString) || data.GetCardID().Contains(searchString))
+            {
+                GameObject dataUI = Instantiate(data_prefab);
+
+                dataUI.GetComponent<DatabaseContent>().SetID(data.GetCardID());
+                dataUI.GetComponent<DatabaseContent>().SetNama(data.GetFullName());
+                dataUI.transform.SetParent(data_content.transform);
+                dataUI.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
             }
-            data.GetComponent<DatabaseContent>().SetID(data_realID);
-            data.GetComponent<DatabaseContent>().SetNama(data_realName);
-            data.transform.SetParent(data_content.transform);
-            data.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
         }
     }
 
